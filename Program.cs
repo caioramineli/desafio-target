f479c45bdfffc3cffd6ba058dd0080215ffc31ab
Caio Ramineli <113688810+caioramineli@users.noreply.github.com>
2024-03-29 18:29:08 -0300

    Desafio da Target implementado

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// EX-1
+int indice = 13;
+int soma = 0;
+int k = 0;
+
+while (k < indice)
+{
+    k++;
+    soma += k;
+}
+
+Console.WriteLine(soma);
+
+
+//EX-2
+Console.WriteLine("Informe um número para verificar se pertence à sequência de Fibonacci:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (VerificarFibonacci(numero))
+{
+    Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+}
+
+static bool VerificarFibonacci(int numero)
+{
+    int anterior = 0;
+    int atual = 1;
+
+    if (numero == 0 || numero == 1)
+    {
+        return true;
+    }
+
+    while (atual <= numero)
+    {
+        int proximo = anterior + atual;
+        if (proximo == numero)
+        {
+            return true;
+        }
+        anterior = atual;
+        atual = proximo;
+    }
+
+    return false;
+}
+
+
+
+// EX-5
+Console.WriteLine("Informe uma string para ser invertida:");
+string texto = Console.ReadLine();
+
+static string InverterString(string texto)
+{
+
+    char[] caracteres = texto.ToCharArray();
+
+    char[] caracteresInvertidos = new char[caracteres.Length];
+
+
+    for (int i = 0; i < caracteres.Length; i++)
+    {
+        caracteresInvertidos[i] = caracteres[caracteres.Length - 1 - i];
+    }
+
+    string textoInvertido = new(caracteresInvertidos);
+
+    return textoInvertido;
+}
+
+Console.WriteLine(InverterString(texto));
